1582c3fef10cb4ed25b75bad6d57adcfaf8484b8
Valery <51917480+dmitriyewich@users.noreply.github.com>
2025-09-07 15:53:39 +0300

    refactor: defer heavy module loading

--- HelperByOrc/init.lua
+++ HelperByOrc/init.lua
@@ -5,16 +5,25 @@ modules.tags           = require('HelperByOrc.tags')
 modules.binder         = require('HelperByOrc.binder')
 modules.unwanted       = require('HelperByOrc.unwanted')
 modules.VIPandADchat   = require('HelperByOrc.VIPandADchat')
-modules.samp           = require('HelperByOrc.samp')
-modules.memory_picture = require('HelperByOrc.memory_picture')
 modules.mimgui_funcs   = require('HelperByOrc.mimgui_funcs')
-modules.my_hooks       = require('HelperByOrc.my_hooks')
 modules.notepad        = require('HelperByOrc.notepad')
 modules.SMIHelp        = require('HelperByOrc.SMIHelp')
 
 for _, m in pairs(modules) do
     if type(m) == 'table' and m.attachModules then
         m.attachModules(modules)
+    end
+end
+
+function modules.loadHeavyModules()
+    modules.samp           = require('HelperByOrc.samp')
+    modules.memory_picture = require('HelperByOrc.memory_picture')
+    modules.my_hooks       = require('HelperByOrc.my_hooks')
+
+    for _, m in pairs(modules) do
+        if type(m) == 'table' and m.attachModules then
+            m.attachModules(modules)
+        end
     end
 end
 
